59bc0a2fabd31d79fb92c999d067794625144b57
Katie Hoffman <ktahoffman@gmail.com>
2014-01-06 16:10:53 -0500

    first attempts at blog creation

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,29 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Posts.all
+		# gives intance variable of all posts
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.find(params[:post])
+		if @post.save
+			redirect_to @post
+		end
+			render new
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+	end
+end
---
+++ app/models/post.rb
@@ -0,0 +1,2 @@
+class Post <AcitveRecord::Base
+end
